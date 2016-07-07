db20e480ad16b64acfe983651d4561b86d7df6a5
m.molodorich <m.molodorich@infinnitysolutions.com>
2016-07-07 17:53:41 +0500

    fix project name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Управдом'
+project = u'Управдом ЖКХ'
 copyright = u'2016, Infinnity Solutions'
 author = u'Infinnity Solutions'
 
